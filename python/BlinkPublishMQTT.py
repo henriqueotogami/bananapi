5e7133bae675385d5b21ec9cd0d40fe1041b587f
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-02-06 22:22:40 -0300

    Ajuste na captura do valor do estado do GPIO 7 lido no comando sudo cat

--- python/BlinkPublishMQTT.py
+++ python/BlinkPublishMQTT.py
@@ -2,7 +2,13 @@ import paho.mqtt.client as mqtt
 import random
 import sys
 import os
+import subprocess
 
+# Henrique Otogami - 06/02/2025
+# https://github.com/henriqueotogami/bananapi
+
+
+# Configuração do cliente
 client_api      = mqtt.CallbackAPIVersion.VERSION2
 client_protocol = mqtt.MQTTv31
 client_security = mqtt.ssl.PROTOCOL_TLS
@@ -11,22 +17,25 @@ client_password = "your_password_here"
 client_url = "your_url_here"
 client_port     = 8883
 
-# device_id       = f'bananapi-{random.randint(0,100)}'
-devide_id       = "bananapi-77"
+device_id       = f'bananapi-{random.randint(0,100)}'
+# device_id       = "bananapi-77"
 target_topic    = "bananapi"
 topic_qos       = 0
 # message         = "Enviando mensagem do script python"
 
 # Callback para conexão
 def on_connect(client, userdata, flags, rc):
-    print(f"Conectado ao broker! Código de retorno: {rc}")
-    client.subscribe(target_topic)  # Inscreve-se em um tópico
+    message = "Conectado ao broker! Código de retorno: " + str(rc)
+    print(message)
+    client.subscribe(target_topic)
 
 # Callback para mensagens recebidas
 def on_message(client, userdata, msg):
-    print(f"Mensagem recebida no tópico {msg.topic}: {msg.payload.decode()}")
+    message = 'Mensagem recebida no tópico ' + str(msg.topic) + ': ' + str(msg.payload.decode())
+    print(message)
 
-
+# Inicio da conexão com o cliente MQTT
+# ============================
 client = mqtt.Client(client_api, client_id = device_id)
 client.on_connect = on_connect
 client.on_message = on_message
@@ -35,38 +44,54 @@ client.tls_set(tls_version = client_security)
 client.username_pw_set(client_username, client_password)
 
 connection  = client.connect(host = client_url, port = client_port)
+# ============================
+# Fim da conexão com o cliente MQTT
 
-print("\nMQTT - Publish")
+print("\nMQTT - Publisher - Início")
 print("\nDevice ID = "  + str(device_id))
 print("\nTopic = "      + str(target_topic))
 print("\nClient = "     + str(client.socket()))
 print("\nConnection = " + str(connection))
 
+# Identificação do sistema operacional
 if(sys.platform == "linux"):
-    print("\nLinux has found")
+    print("\nLinux foi identificado")
 
-os.system('sudo echo 7 > /sys/class/gpio/export')
-os.system('sudo echo out > /sys/class/gpio/gpio7/direction')
+    # Disponibilização do pino do LED para utilização
+    os.system('sudo echo 7 > /sys/class/gpio/export')
+    os.system('sudo echo out > /sys/class/gpio/gpio7/direction')
 
-while(True):
-    switch_led = input("Turn on (1) / off (0) LED = ")
-    if(switch_led == "quit"): break
+    while(True):
+        # Usuário informa se vai acender ou apagar o LED ou se vai sair do loop
+        switch_led = input("Turn on (1) / off (0) LED = ")
 
-    turn_on_or_off = "1" if switch_led == "1" else "0"
-    os.system(f'sudo echo {turn_on_or_off} > /sys/class/gpio/gpio7/value')
+        if(switch_led == "quit"): break
 
-    is_led_on_or_off = os.system('sudo cat /sys/class/gpio/gpio7/value')
-    message_to_broker = "led on" if is_led_on_or_off == "1" else "led off"
+        # Formatação do comando shell para acender ou apagar o LED
+        turn_on_or_off = "1" if switch_led == "1" else "0"
+        set_gpio7_value = 'sudo echo ' + str(turn_on_or_off) + ' > /sys/class/gpio/gpio7/value'
+        os.system(set_gpio7_value)
 
-    print(message_to_broker)
-    answer      = client.publish(topic = target_topic, payload = message_to_broker, qos = topic_qos, retain = True)
-    print("\nAnswer = "     + str(answer.is_published()))
-    print("\nMensagem enviada para o Broker!")
+        # Consulta do estado do LED para notificar o cliente do MQTT
+        get_gpio7_value = 'sudo cat /sys/class/gpio/gpio7/value'
+        try:
+            # Armazena o estado do LED
+            is_led_on_or_off = subprocess.check_output(get_gpio7_value, shell=True, text=True).strip()
+            print("O valor do GPIO7 é: " + is_led_on_or_off)
+        except subprocess.CalledProcessError as e:
+            print("Erro ao executar o comando")
 
-# client.loop_forever()
-# sudo pip3 install paho-mqtt
+        # Formatação da mensagem para o Broker
+        message_to_broker = "led on" if is_led_on_or_off == "1" else "led off"
 
-# scp /Users/henriquematheusalvespereira/Documents/Github/bananapi/python/PublishMQTT.py pi@bananapi-zero.local:otogamidev/
+        # Envio da mensagem e Resposta do Broker
+        answer = client.publish(topic = target_topic, payload = message_to_broker, qos = topic_qos, retain = True)
 
-# python3 -m pip install --upgrade pip
-# sudo apt install python 3.10
+        print("\nMensagem para o Broker = " + message_to_broker)
+        print("\nMensagem foi publicada no Broker = " + str(answer.is_published()))
+        print("\nMensagem enviada com sucesso para o Broker!")
+else:
+    print("\nSistema operacional = " + sys.platform)
+    print("\nImplementação suporta apenas o Linux")    
+
+print("\nMQTT - Publisher - Fim")
